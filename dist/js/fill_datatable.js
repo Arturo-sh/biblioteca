971d8363fea5f6d442e3aa49edc23d640186c307
Arturo -sh <salashernandezarturo1512@gmail.com>
2023-10-17 03:46:01 -0600

    Feature -> Módulo préstamos con ajax y ajax.reload

--- dist/js/fill_datatable.js
+++ dist/js/fill_datatable.js
@@ -1,21 +1,22 @@
 function fill_tbl_loans() {
   let cols = [
-    { data: "id_usuario" },
-    { data: "usuario" },
+    { data: "id_prestamo" },
+    { data: "nombre_alumno" },
+    { data: "titulo_libro" },
+    { data: "unidades_prestamo" },
+    { data: "fecha_prestamo" },
+    { data: "fecha_entrega" },
     { data: "nombre_usuario" },
-    { data: "telefono_usuario" },
-    { data: "correo_usuario" },
-    { data: "creacion_cuenta" },
     { 
-      data: "estado_usuario",
+      data: "estado_prestamo",
       render: function (data, type) {
         if (type === 'display') {
           let template = `
           <td class='text-center'>
-            <span class='badge bg-danger'>${data}</span>
+            <span class='badge bg-primary'>${data}</span>
           </td>`;
 
-          if (data == "Activo") {
+          if (data == "Entregado") {
             template = `
             <td class='text-center'>
               <span class='badge bg-success'>${data}</span>
@@ -27,84 +28,20 @@ function fill_tbl_loans() {
       } 
     },
     {
-      data: "rol_usuario",
-      render: function (data, type, row, meta) {
-        if (type === 'display') {
-          let template = `
-          <button id='${row.id_usuario}' mod='usuarios' class='btn btn-sm btn-primary btn-edit' data-toggle='modal' data-target='#modal-default'>
-            <i class='fas fa-pen'></i>
-          </button>`;
-
-          if (data != "Admin") {
-            template = `
-            <button id='${row.id_usuario}' mod='usuarios' class='btn btn-sm btn-primary btn-edit' data-toggle='modal' data-target='#modal-default'>
-              <i class='fas fa-pen'></i>
-            </button>
-            <button id='${row.id_usuario}' mod='usuarios' class='btn btn-sm btn-danger btn-delete'>
-              <i class='fas fa-trash'></i>
-            </button>`;
-          }
-          return template;
-        }
-        return data;
-      }
-    }];
-  return cols;
-}
-
-function fill_tbl_users() {
-  let cols = [
-    { data: "id_usuario" },
-    { data: "usuario" },
-    { data: "nombre_usuario" },
-    { data: "telefono_usuario" },
-    { data: "correo_usuario" },
-    { data: "creacion_cuenta" },
-    { 
-      data: "estado_usuario",
+      data: "id_prestamo",
       render: function (data, type) {
         if (type === 'display') {
-          let template = `
-          <td class='text-center'>
-            <span class='badge bg-danger'>${data}</span>
-          </td>`;
- 
-          if (data == "Activo") {
-            template = `
-            <td class='text-center'>
-              <span class='badge bg-success'>${data}</span>
-            </td>`;
-          }
-
-          return template;
-        }
- 
-        return data;
-      } 
-    },
-    {
-      data: "rol_usuario",
-      render: function (data, type, row, meta) {
-        if (type === 'display') {
-          let template = `
-          <button id='${row.id_usuario}' mod='usuarios' class='btn btn-sm btn-primary btn-edit' data-toggle='modal' data-target='#modal-default'>
-            <i class='fas fa-pen'></i>
-          </button>`;
- 
-        if (data != "Admin") {
           template = `
-          <button id='${row.id_usuario}' mod='usuarios' class='btn btn-sm btn-primary btn-edit' data-toggle='modal' data-target='#modal-default'>
+          <button id='${data}' mod='prestamos' class='btn btn-sm btn-primary btn-edit'>
             <i class='fas fa-pen'></i>
           </button>
-          <button id='${row.id_usuario}' mod='usuarios' class='btn btn-sm btn-danger btn-delete'>
+          <button id='${data}' mod='prestamos' class='btn btn-sm btn-danger btn-delete'>
             <i class='fas fa-trash'></i>
           </button>`;
         }
         return template;
       }
-      return data;
-    }
-  }];
+    }];
   return cols;
 }
 
@@ -146,9 +83,62 @@ function fill_tbl_students() {
             <i class='fas fa-trash'></i>
           </button>`;
         }
-
         return template;
       }
     }];
+  return cols;
+}
+
+function fill_tbl_users() {
+  let cols = [
+    { data: "id_usuario" },
+    { data: "usuario" },
+    { data: "nombre_usuario" },
+    { data: "telefono_usuario" },
+    { data: "correo_usuario" },
+    { data: "creacion_cuenta" },
+    { 
+      data: "estado_usuario",
+      render: function (data, type) {
+        if (type === 'display') {
+          let template = `
+          <td class='text-center'>
+            <span class='badge bg-danger'>${data}</span>
+          </td>`;
+ 
+          if (data == "Activo") {
+            template = `
+            <td class='text-center'>
+              <span class='badge bg-success'>${data}</span>
+            </td>`;
+          }
+          return template;
+        }
+        return data;
+      } 
+    },
+    {
+      data: "rol_usuario",
+      render: function (data, type, row, meta) {
+        if (type === 'display') {
+          let template = `
+          <button id='${row.id_usuario}' mod='usuarios' class='btn btn-sm btn-primary btn-edit' data-toggle='modal' data-target='#modal-default'>
+            <i class='fas fa-pen'></i>
+          </button>`;
+ 
+        if (data != "Admin") {
+          template = `
+          <button id='${row.id_usuario}' mod='usuarios' class='btn btn-sm btn-primary btn-edit' data-toggle='modal' data-target='#modal-default'>
+            <i class='fas fa-pen'></i>
+          </button>
+          <button id='${row.id_usuario}' mod='usuarios' class='btn btn-sm btn-danger btn-delete'>
+            <i class='fas fa-trash'></i>
+          </button>`;
+        }
+        return template;
+      }
+      return data;
+    }
+  }];
   return cols;
 }
